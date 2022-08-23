2d16e2cfc301f8a16e4cbfe638edafc6ad6679f6
yulia <kimkaaa@mail.ru>
2022-08-23 22:22:39 +0300

    Task8

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -3,8 +3,8 @@
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 if (number % 2 == 0)
-Console.Write($"Да");
+    Console.Write($"Да");
 else
-Console.Write($"Нет");
+    Console.Write($"Нет");
 
 
